966ac1277fd79d1e4d06c62c4084463d71d9400f
Varada Raja Reddy Peddyreddy <141263045+Varada-Raja-Reddy-Peddyreddy@users.noreply.github.com>
2024-06-04 14:14:31 +0530

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,15 @@
+
+public class Human {
+        String name;
+        int age;
+        int weight;
+   Human(String name,int age,int weight){
+    this.name = name;
+    this.age= age;
+    this.weight =weight;
+
+    
+
+
+   }
+}
---
+++ Human_Main.java
@@ -0,0 +1,11 @@
+public class Human_Main {
+    public static void main(String[] args) {
+        Human human1 = new Human("TATA",80,70);
+
+        System.out.println(human1.name);
+        System.out.println(human1.age);
+        System.out.println(human1.weight);
+
+    }
+    
+}
